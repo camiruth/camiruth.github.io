f23736a7abd6db769348460165c987eef66869ce
camiruth <camiruth.anderson@gmail.com>
2020-01-15 13:27:48 -0700

    Lesson 2

---
+++ lesson2/js/lastupdated.js
@@ -0,0 +1,13 @@
+let d = new Date();
+let y = d.getFullYear();
+document.getElementById("currentyear").textContent = y;
+
+let m = new Date(document.lastModified);
+let month = m.getMonth() + 1;
+let day = m.getDate();
+let year = m.getFullYear();
+let hour = m.getHours();
+let minutes = m.getMinutes();
+let seconds = m.getSeconds();
+let dateString = month + "/" + day + "/" + year + " " + hour + ":" + minutes + ":" + seconds;
+document.getElementById("lastupdated").textContent = dateString;
