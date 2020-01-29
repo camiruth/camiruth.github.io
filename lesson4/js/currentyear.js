e05c12b8fc1e7b733fe5e4d2985cf83b8173fa66
camiruth <camiruth.anderson@gmail.com>
2020-01-29 10:08:20 -0700

    start of preston page

---
+++ lesson4/js/currentyear.js
@@ -0,0 +1,7 @@
+let m = new Date();
+let month = m.getMonth() + 1;
+let day = m.getDate();
+let year = m.getFullYear();
+let dateString = month + "/" + day + "/" + year + " ";
+document.getElementById("currentyear").textContent = dateString;
+
