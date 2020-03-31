8a78bb744edfcbff38b2428e17804edd3a9c478b
camiruth <camiruth.anderson@gmail.com>
2020-03-31 10:08:58 -0600

    Json

--- salmonriver/js/guidesinfo.js
+++ salmonriver/js/guidesinfo.js
@@ -17,26 +17,26 @@ fetch(requestURL)
                 let div = document.createElement('div');
 
                 let h2 = document.createElement('h2');
-                let h3 = document.createElement('h3');
                 let p1 = document.createElement('p');
                 let p2 = document.createElement('p');
                 let p3 = document.createElement('p');
+                let h3 = document.createElement('h3');
                 let img = document.createElement('img');
 
 
                 h2.textContent = guides[i].name;
-                h3.textContent = guides[i].biography;
                 p1.textContent = "Certification Level: " + guides[i].certLevel;
                 p2.textContent = "Years Experience: " + guides[i].yearsExp;
                 p3.textContent = "Email: " + guides[i].emailAddress;
+                h3.textContent = guides[i].biography;
                 img.setAttribute('src', 'images/' + guides[i].photo);
                 img.setAttribute('alt', guides[i].name);
 
                 div.appendChild(h2);
-                div.appendChild(h3);
                 div.appendChild(p1);
                 div.appendChild(p2);
                 div.appendChild(p3);
+                div.appendChild(h3);
 
                 card.appendChild(div);
                 card.appendChild(img);
